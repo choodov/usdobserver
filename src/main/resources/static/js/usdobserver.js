cf63fd8ff8a6573b9ebd48c1f58f2df25dc52bf7
Aleksandr Chudov <choodov@gmail.com>
2017-06-05 22:13:11 +0300

    Add simple chart realization.

--- src/main/resources/static/js/usdobserver.js
+++ src/main/resources/static/js/usdobserver.js
@@ -20,4 +20,44 @@ $(document).ready(function () {
         sServerMethod: "POST"
     });
 
+//    chart creating
+    var ctx = document.getElementById("rates-chart").getContext('2d');
+    var ratesChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["2017-05-02", "2017-05-04", "2017-05-05", "2017-05-08", "2017-05-09", "2017-05-10"],
+            datasets: [{
+                label: 'USD rates',
+                data: [3.8675, 3.863, 3.849, 3.8391, 3.8741, 3.8777],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            },
+            maintainAspectRatio: false
+        }
+    });
+
 });
